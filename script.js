91c9ea50a6a9581c6cb320462afcffea1fd66d8d
Greg <137340834+GregoryMinella@users.noreply.github.com>
2025-08-24 19:38:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode()
+{
+    const html = document.documentElement;
+    html.classList.toggle("light")
+
+    const img = document.querySelector("#profile img");
+
+    if (html.classList.contains("light"))
+    {
+        img.setAttribute("src", "/imagens/perfil - claro.jpg");
+    }
+    else
+    {
+        img.setAttribute("src", "/imagens/perfil - escuro.jpg");
+    }    
+
+}
